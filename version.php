31f4b1713831f902c126c5d645cfc8b0cc46dec5
David Bogner <davidbogner@gmail.com>
2013-02-15 18:56:06 +0100

    release number added

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
     $plugin->version = 2013012300;  // YYYYMMDDHH (year, month, day, 24-hr time)
     $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
-    
+    $plugin->release = '1.0 Build: 2013012300';
 ?>
