63ecc55f83a1ce359f06621f3e2effdc76e61f4d
David Bogner <davidbogner@gmail.com>
2013-03-14 14:23:57 +0100

    version number updated

--- version.php
+++ version.php
@@ -1,5 +1,27 @@
 <?php
-    $plugin->version = 2013012300;  // YYYYMMDDHH (year, month, day, 24-hr time)
+// This moodle block is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This software is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// GNU General Public License V.3
+// <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details
+ *
+ * @package    block
+ * @subpackage simple_map
+ * @copyright  2013 Georg Maißer, David Bogner, http://www.edulabs.org
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+    $plugin->version = 2013031400;  // YYYYMMDDHH (year, month, day, 24-hr time)
     $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
     $plugin->release = '1.0 Build: 2013012300';
 ?>
